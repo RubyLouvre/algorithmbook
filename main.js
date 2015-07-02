a8993f5f3e1860988fa839e58529bda2fc1c7259
RubyLouvre <cheng19840218@gmail.com>
2015-07-02 16:05:16 +0800

    add

---
+++ main.js
@@ -0,0 +1,8 @@
+var utils = require("./utils")
+
+var lib = {
+    
+}
+utils.mix(lib, utils)
+
+window.QAPP  = lib
---
+++ qapp.js
@@ -0,0 +1,230 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	var utils = __webpack_require__(1)
+
+	var lib = {
+	    
+	}
+	utils.mix(lib, utils)
+
+	window.QAPP  = lib
+
+
+/***/ },
+/* 1 */
+/***/ function(module, exports, __webpack_require__) {
+
+	
+	var rword = /[^, ]+/g //切割字符串为一个个小块，以空格或豆号分开它们，结合replace实现字符串的forEach
+	var oproto = Object.prototype
+	var ohasOwn = oproto.hasOwnProperty
+	var serialize = oproto.toString
+	var class2type = {}
+	"Boolean Number String Function Array Date RegExp Object Error".replace(rword, function (name) {
+	    class2type["[object " + name + "]"] = name.toLowerCase()
+	})
+
+
+	function noop() {
+	}
+
+	function oneObject(array, val) {
+	    if (typeof array === "string") {
+	        array = array.match(rword) || []
+	    }
+	    var result = {},
+	            value = val !== void 0 ? val : 1
+	    for (var i = 0, n = array.length; i < n; i++) {
+	        result[array[i]] = value
+	    }
+	    return result
+	}
+
+	var isPlainObject = function (obj) {
+	    // 简单的 typeof obj === "object"检测，会致使用isPlainObject(window)在opera下通不过
+	    return serialize.call(obj) === "[object Object]" && Object.getPrototypeOf(obj) === oproto
+	}
+
+	var isFunction = function (fn) {
+	    return serialize.call(fn) === "[object Function]"
+	}
+
+
+	var mix = function () {
+	    var options, name, src, copy, copyIsArray, clone,
+	            target = arguments[0] || {},
+	            i = 1,
+	            length = arguments.length,
+	            deep = false
+
+	    // 如果第一个参数为布尔,判定是否深拷贝
+	    if (typeof target === "boolean") {
+	        deep = target
+	        target = arguments[1] || {}
+	        i++
+	    }
+
+	    //确保接受方为一个复杂的数据类型
+	    if (typeof target !== "object" && !isFunction(target)) {
+	        target = {}
+	    }
+
+	    //如果只有一个参数，那么新成员添加于mix所在的对象上
+	    if (i === length) {
+	        target = this
+	        i--
+	    }
+
+	    for (; i < length; i++) {
+	        //只处理非空参数
+	        if ((options = arguments[i]) != null) {
+	            for (name in options) {
+	                src = target[name]
+
+	                // 防止环引用
+	                if (target === copy) {
+	                    continue
+	                }
+	                if (deep && copy && (isPlainObject(copy) || (copyIsArray = Array.isArray(copy)))) {
+
+	                    if (copyIsArray) {
+	                        copyIsArray = false
+	                        clone = src && Array.isArray(src) ? src : []
+
+	                    } else {
+	                        clone = src && isPlainObject(src) ? src : {}
+	                    }
+
+	                    target[name] = mix(deep, clone, copy)
+	                } else if (copy !== void 0) {
+	                    target[name] = copy
+	                }
+	            }
+	        }
+	    }
+	    return target
+	}
+
+	/*判定是否类数组，如节点集合，纯数组，arguments与拥有非负整数的length属性的纯JS对象*/
+	function isArrayLike(obj) {
+	    if (!obj)
+	        return false
+	    var n = obj.length
+	    if (n === (n >>> 0)) { //检测length属性是否为非负整数
+	        var type = serialize.call(obj).slice(8, -1)
+	        if (/(?:regexp|string|function|window|global)$/i.test(type))
+	            return false
+	        if (type === "Array")
+	            return true
+	        try {
+	            if ({}.propertyIsEnumerable.call(obj, "length") === false) { //如果是原生对象
+	                return  /^\s?function/.test(obj.item || obj.callee)
+	            }
+	            return true
+	        } catch (e) { //IE的NodeList直接抛错
+	            return !obj.window //IE6-8 window
+	        }
+	    }
+	    return false
+	}
+	if (true) {
+	    module.exports = {
+	        noop: noop,
+	        type: function (obj) { //取得目标的类型
+	            if (obj == null) {
+	                return String(obj)
+	            }
+	            // 早期的webkit内核浏览器实现了已废弃的ecma262v4标准，可以将正则字面量当作函数使用，因此typeof在判定正则时会返回function
+	            return typeof obj === "object" || typeof obj === "function" ?
+	                    class2type[serialize.call(obj)] || "object" :
+	                    typeof obj
+	        },
+	        oneObject: oneObject,
+	        isFunction: isFunction,
+	        isPlainObject: isPlainObject,
+	        /*遍历数组与对象,回调的第一个参数为索引或键名,第二个或元素或键值*/
+	        each: function (obj, fn) {
+	            if (obj) { //排除null, undefined
+	                var i = 0
+	                if (isArrayLike(obj)) {
+	                    for (var n = obj.length; i < n; i++) {
+	                        if (fn(i, obj[i]) === false)
+	                            break
+	                    }
+	                } else {
+	                    for (i in obj) {
+	                        if (obj.hasOwnProperty(i) && fn(i, obj[i]) === false) {
+	                            break
+	                        }
+	                    }
+	                }
+	            }
+	        },
+	        Array: {
+	            /*只有当前数组不存在此元素时只添加它*/
+	            ensure: function (target, item) {
+	                if (target.indexOf(item) === -1) {
+	                    return target.push(item)
+	                }
+	            },
+	            /*移除数组中指定位置的元素，返回布尔表示成功与否*/
+	            removeAt: function (target, index) {
+	                return !!target.splice(index, 1).length
+	            },
+	            /*移除数组中第一个匹配传参的那个元素，返回布尔表示成功与否*/
+	            remove: function (target, item) {
+	                var index = target.indexOf(item)
+	                if (~index)
+	                    return avalon.Array.removeAt(target, index)
+	                return false
+	            }
+	        }
+	    }
+	}
+
+/***/ }
+/******/ ]);
